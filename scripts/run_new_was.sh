566d15afdd8069afe5b491dd63063015a299ed99
jsjune <avraskio@gmail.com>
2022-05-02 21:21:44 +0900

    update

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -22,7 +22,7 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/ec2-user/com.eungsoo.blog/build/libs/* > /home/ubuntu/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ubuntu/ec2-user/com-eungsoo-blog/build/libs/* > /home/ubuntu/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
 
